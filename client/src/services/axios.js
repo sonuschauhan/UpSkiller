68e4da0ddb005bfebc239a1f45170890966799cc
sonu chauhan <sonusingha37fw@gmail.com>
2025-08-08 03:08:02 +0530

    bug

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://upskiller-server.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
